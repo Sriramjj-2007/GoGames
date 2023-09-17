fc2a76bc9f4013c93c138ba4d0fa8eea4cd753d5
SRIRAM J J <84657915+Sriramjj-2007@users.noreply.github.com>
2023-09-17 18:13:39 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,11 @@
+var block = document.getElementById('block')
+document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    // var code = event.code;
+
+    switch (name) {
+        default:
+            console.log(name);
+            break;
+    }
+}, false);
